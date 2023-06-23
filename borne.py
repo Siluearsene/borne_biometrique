1e1476b98ebcc01337a52467236873f070525ccd
Hydra16LeGrand <baradjiamara17@gmail.com>
2023-06-23 07:55:09 +0000

    premier commit

---
+++ borne.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template,request
+import secrets 
+import psycopg2
+
+app = Flask(__name__)
+
+secret_key = secrets.token_hex(16)
+
+@app.route("/",methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        adresse_ip = request.form.get('addresse_ip')
+        port = request.form.get('port')
+        lieu = request.form.get('lieu')
+        print('kw', adresse_ip, port, lieu)
+        # Connexion à la base de données
+        conn = psycopg2.connect(
+            host='localhost',
+            port=5432,
+            dbname='borne',
+            user='borne',
+            password='1234'
+        )
+        cursor = conn.cursor()
+
+        # Requête d'insertion
+        insert_query = "INSERT INTO borneregister (addresse_ip, port, lieu) VALUES (%s, %s, %s)"
+        values = (adresse_ip, port, lieu)
+
+        # Exécution de la requête
+        cursor.execute(insert_query, values)
+        conn.commit()
+
+        # Fermeture de la connexion à la base de données
+        cursor.close()
+        conn.close()
+
+        return render_template('index.html', message='Enregistrement effectué avec succès')
+
+    # Si la méthode de requête est GET, afficher simplement le formulaire
+    return render_template('index.html')
+
+# ...
+
+
+@app.route("/home")
+def acceuil():
+    return render_template('index.html')
+@app.route("/crud")
+def acceuil():
+    return render_template('crud.html')
+
+
+
+app.config['SECRET_KEY'] = secret_key
+
+# Configuration de la base de données PostgreSQL
+db_connection = {
+    'host': 'localhost',
+    'port': 5432,
+    'dbname': 'borne',
+    'user': 'borne',
+    'password': '1234'
+}
